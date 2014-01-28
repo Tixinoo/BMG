bc83c31501f41942f07776fe95683e6a9ad67ce0
-- <--@192.168.229.185>
2014-01-28 15:51:22 +0100

    User Package

---
+++ BMG/src/tests/Tests.java
@@ -0,0 +1,12 @@
+package tests;
+
+import user.User;
+
+public class Tests 
+{
+    public static void main(String[] args)
+    {
+    User user = new User(5,"Antoine","NONO","IUT Charlo","kikoolol57@hotmail.fr","comme_MA_b***");
+    String s = user.insert();
+    }
+}
