552812e804d4dfeb0834652af60aa24ecb5866ed
Tixinoo <antoine.nosal@gmail.com>
2014-01-30 02:25:32 +0100

    // Tixinoo

--- BMG/src/model/QuestionCustom.java
+++ BMG/src/model/QuestionCustom.java
@@ -9,22 +9,22 @@ public class QuestionCustom<SolutionType> extends Question {
     // Inherited
     
     /**
-     * List of the name of the attributes
+     * List of the name of the attributes.
      */
     private ArrayList<String> names;
 
     /**
-     * List of the types of the attributes
+     * List of the types of the attributes.
      */
     private ArrayList<String> types;
 
     /**
-     * List of the values of the attributes
+     * List of the values of the attributes.
      */
     private ArrayList<Object> values;
 
     /**
-     * Solution of the question
+     * Solution of the question.
      */
     private SolutionType solution;
 
@@ -33,26 +33,96 @@ public class QuestionCustom<SolutionType> extends Question {
     // ---- CONSTRUCTORS ----
     
     /**
-     * This constructor creates the simplest question for a calculation
+     * This constructor creates an empty question.
      */
     public QuestionCustom() {
         super();
-
+        this.text = "Answer.";
+        this.difficulty = 0;
+        this.names = new ArrayList<String>();
+        this.types = new ArrayList<String>();
+        this.values = new ArrayList<Object>();
+        this.solution = null;
     }
 
     /**
-     * This constructor creates a question with the text given in parameter
+     * This constructor creates a question,
+     * with the text given in parameter.
      */
     public QuestionCustom(String QCtext) {
-
+        super();
+        if (QCtext != null) {
+            this.text = QCtext;
+        } else {
+            this.text = "...";
+        }
+        this.difficulty = 0;
+        this.names = new ArrayList<String>();
+        this.types = new ArrayList<String>();
+        this.values = new ArrayList<Object>();
+        this.solution = null;
+    }
+    
+    /**
+     * This constructor creates a question,
+     * with the text and the solution given in parameters.
+     */
+    public QuestionCustom(String QCtext, SolutionType QCsolution) {
+        super();
+        if (QCtext != null) {
+            this.text = QCtext;
+        } else {
+            this.text = "...";
+        }
+        this.difficulty = 0;
+        this.names = new ArrayList<String>();
+        this.types = new ArrayList<String>();
+        this.values = new ArrayList<Object>();
+        this.solution = QCsolution;
     }
 
     /**
-     * This constructor creates a question with the text and the difficulty
-     * given in parameters
+     * This constructor creates a question,
+     * with the text and the difficulty given in parameters.
      */
     public QuestionCustom(String QCtext, int QCdifficulty) {
-
+        super();
+        if (QCtext != null) {
+            this.text = QCtext;
+        } else {
+            this.text = "...";
+        }
+        if (QCdifficulty >= 0) {
+            this.difficulty = QCdifficulty;
+        } else {
+            this.difficulty = 0;
+        }
+        this.names = new ArrayList<String>();
+        this.types = new ArrayList<String>();
+        this.values = new ArrayList<Object>();
+        this.solution = null;
+    }
+    
+    /**
+     * This constructor creates a question,
+     * with the text, the difficulty and the solution given in parameters.
+     */
+    public QuestionCustom(String QCtext, int QCdifficulty, SolutionType QCsolution) {
+        super();
+        if (QCtext != null) {
+            this.text = QCtext;
+        } else {
+            this.text = "...";
+        }
+        if (QCdifficulty >= 0) {
+            this.difficulty = QCdifficulty;
+        } else {
+            this.difficulty = 0;
+        }
+        this.names = new ArrayList<String>();
+        this.types = new ArrayList<String>();
+        this.values = new ArrayList<Object>();
+        this.solution = QCsolution;
     }
 
     // ----------------------
@@ -60,21 +130,6 @@ public class QuestionCustom<SolutionType> extends Question {
     // ------- METHODS ------
     
     // Inherited
-    
-    /**
-     * Generate a random question with an equation
-     */
-    public void generate() {
-
-    }
-
-    /**
-     * Generate a random question with an equation with the length given in
-     * parameter
-     */
-    public void generate(int QClength) {
-
-    }
 
     /**
      * True if the object given in parameter equals the solution
@@ -89,13 +144,18 @@ public class QuestionCustom<SolutionType> extends Question {
         return res;
     }
 
+    public void setSolution(SolutionType solution) {
+        this.solution = solution;
+    }    
+    
     /**
-     * Display a question with an equation
+     * Display a question custom.
      */
     public String toString() {
         String res = "		QuestionCustom";
-        res = res + "\n			Text: " + this.text;
-        res = res + "\n			Difficulty: " + this.difficulty;
+        res = res + "\n                 Text: " + this.text;
+        res = res + "\n                 Difficulty: " + this.difficulty;
+        res = res + "\n                 Solution: " + this.solution;
         return res;
     }
 
