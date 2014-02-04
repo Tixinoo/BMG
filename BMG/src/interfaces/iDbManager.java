f6281cdc3f99b712891b8716c27ec5113e1e4725
-- <--@192.168.229.18>
2014-02-04 13:51:42 +0100

    ajout de l'interface 'iDbManager' dans le package 'interfaces' ajout des implementations de cette interface pour chacune des classes du package 'user'

--- BMG/src/interfaces/iDbManager.java
+++ BMG/src/interfaces/iDbManager.java
@@ -7,7 +7,4 @@ public interface iDbManager
     public boolean insert(BaseSetting bs);
     public boolean update(BaseSetting bs);
     public boolean delete(BaseSetting bs);
-	
-//    public Object findById(int id, BaseSetting bs);
-//    public Object[] findAll(BaseSetting bs);
 }
