f3a0e9c8574796b45a27b225173a5e8df873e3cd
-- <--@192.168.1.13>
2014-02-08 22:56:46 +0100

    apporte quelques modifications aux tests dans Others_Tests

--- BMG/src/tests/Others_Tests.java
+++ BMG/src/tests/Others_Tests.java
@@ -4,9 +4,9 @@ import model.Wording;
 
 public class Others_Tests 
 {
-    @SuppressWarnings("empty-statement")
-    public static void main(String[] args)
-    {	    
+
+    public static void main(String[] args) 
+    {
 	double de = 3.50;
 	int it = 666;
 	String sg = "hell";
@@ -20,38 +20,38 @@ public class Others_Tests
 //	res = Wording.getType(cr);	System.out.println(res);
 	
 	System.out.println();
-	
+
 	System.out.println("DEBUT TEST ENCODE_VALUES");
-	
+
 	String big_s_encode = "";
-	Object[] t_obj_encode = {de,it,sg,cr};
-	
-	Wording w_encode = new Wording("texte...",new Object[4]);
-	
+	Object[] t_obj_encode = {de, it, sg, cr};
+
+	Wording w_encode = new Wording("texte...", new Object[4]);
+
 	w_encode.setValues(t_obj_encode);
-	
+
 	big_s_encode = w_encode.encodeValues();
-	
-	System.out.println("Obtenu :  "+big_s_encode);
-	System.out.println("Attendu : "+"dbl:int:str:chr><3.50:666:hell:!");
-	
+
+	System.out.println("Obtenu :  " + big_s_encode);
+	System.out.println("Attendu : " + "dbl:int:str:chr><3.50:666:hell:!");
+
 	System.out.println("FIN TEST ENCODE_VALUES");
-	
+
 	System.out.println();
-	
+
 	System.out.println("DEBUT TEST DECODE_VALUES");
-	
-	Wording w_decode = new Wording("texte...",new Object[4]);
-	
+
+	Wording w_decode = new Wording("texte...", new Object[4]);
+
 	String big_s_decode = "dbl:int:str:chr><3.50:666:hell:!";
 	Object[] t_obj_decode = null;
-	
+
 	t_obj_decode = Wording.decodeValues(big_s_decode);
 	w_decode.setValues(t_obj_decode);
-	
-	System.out.println("Obtenu :  "+w_decode.encode());
-	System.out.println("Attendu : "+"#Wording<-1><$<texte...>$><dbl:int:str:chr><3.50:666:hell:!>");
-	
+
+	System.out.println("Obtenu :  " + w_decode.encode());
+	System.out.println("Attendu : " + "#Wording<-1><$<texte...>$><dbl:int:str:chr><3.50:666:hell:!>");
+
 	System.out.println("FIN TEST DECODE_VALUES");
 
     }
