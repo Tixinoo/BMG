2e6669a89172fd5d065504492d03d7f547b3d6cf
blaise <blaise@blaise-p6-2050fr>
2014-02-10 21:15:18 +0100

    Add comments.

--- BMG/src/view/BmgFrame.java
+++ BMG/src/view/BmgFrame.java
@@ -5,30 +5,32 @@
  */
 package view;
 
+// Here all imports needed for this class.
 import database.BaseSetting;
-import java.awt.Color;
 import java.awt.Dimension;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 //Bonjour
 /**
  * Class BmgFrame
  *
- * @author blaise
+ * @author Maxime Blaise
  */
-public class BmgFrame extends JFrame {
-
+public final class BmgFrame extends JFrame {
+    //Needed fields.
     BaseSetting bs = new BaseSetting();
     int width;
     int height;
 
+    //Menu bar and label in south, generate only once.
     BmgPanelMenu panMenu;
     BmgPanelSouth panSouth;
 
+    //Main pan!!
     BmgPanel pan;
 
+    // All panel needed for our program.
     static JPanel panMain;
     static JPanel panSettings;
     static JPanel panSignup;
@@ -39,21 +41,28 @@ public class BmgFrame extends JFrame {
      * Constructor who create the main window of BMG.
      *
      * @param string
+     * @param width
+     * @param height
      */
     public BmgFrame(String string, int width, int height) {
         super(string);
         this.width = width;
         this.height = height;
 
+        //Create these panel.
         panMenu = new BmgPanelMenu(fen());
         panSouth = new BmgPanelSouth(width);
 
+        //Create all panels for our program, once at beginning only !
         setAllPanels();
+        
+        //Frame settings
         setFrameSettings();
     }
 
     /**
      * This method aims at creating all panels at the beginning.
+     * 
      */
     private void setAllPanels() {
         panMain = BmgCreatePanel.createMainPanel(fen(), bs, width, height);
@@ -67,20 +76,33 @@ public class BmgFrame extends JFrame {
      * Method who change window's settings.
      */
     private void setFrameSettings() {
+        
+        //All settings
         this.setPanel(panMain);
         this.setLocation(200, 100);
         this.setPreferredSize(new Dimension(width, height));
         this.setResizable(false);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
         this.pack();
         this.setVisible(true);
-        new BmgFrameBegin(bs, fen());
+        
+        //Begin frame, just once !
+        BmgFrameBegin bmgFrameBegin = new BmgFrameBegin(bs, fen());
     }
 
+    /**
+     * Return this frame.
+     * @return 
+     */
     public BmgFrame fen() {
         return this;
     }
 
+    /**
+     * Change only the center panel !
+     * @param panel 
+     */
     public void setPanel(JPanel panel) {
 
         this.setContentPane(new BmgPanel(fen(), panel));
