bdf2427f42bf8c5d3756882bc12abeac23cd1867
-- <--@192.168.76.31>
2014-02-14 17:02:48 +0100

    ajoute les méthodes 'insert', 'update', 'delete', 'findById' et 'findByIds' dans la classe 'Practice' du package 'model'

--- BMG/src/model/Practice.java
+++ BMG/src/model/Practice.java
@@ -7,12 +7,15 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Date;
 import java.util.ArrayList;
+import user.User;
 
 /**
  *
  * @author tixinoo
  */
 public class Practice {
+    
+    private int id_p;
     
     private int id_u;
     
@@ -98,31 +101,43 @@ public class Practice {
         this.execution_time = execution_time;
     }
     
+    public int getId_p()
+    {
+	return id_p;
+    }
+    
     public boolean insert(BaseSetting bs)
     {
 	Connection connection = bs.getConnection();
 	
-//	try {
-//            String query = "INSERT INTO Practice (id_u,id_e,execution_date,execution_time,success,wrong_answers) VALUES (?,?,?,?,?,?)";
-//            PreparedStatement p_statement = connection.prepareStatement(query, PreparedStatement.RETURN_GENERATED_KEYS);
-//            p_statement.setInt(1, this.id_u);
-//            p_statement.setInt(2, this.id_e);
-//            p_statement.executeUpdate();
-//            ResultSet rs = p_statement.getGeneratedKeys();
-//
-//            if (rs.next()) {
-//                this.id_u = rs.getInt(1);
-//		this.id_e = rs.getInt(2);
-//            }
-//            
-//            return true;
-//
-//        }  
-//	catch (SQLException sqle) 
-//	{
-//	    System.out.println("ERREUR");
-//	    sqle.printStackTrace();
-//	}
+	try {
+            if (User.findById(this.id_u, bs) != null && Exercise.findById(id_e, bs) != null)
+	    {
+		
+	    String query = "INSERT INTO Practice (id_u,id_e,execution_date,execution_time,success,wrong_answers) VALUES (?,?,?,?,?,?)";
+            PreparedStatement p_statement = connection.prepareStatement(query, PreparedStatement.RETURN_GENERATED_KEYS);
+            p_statement.setInt(1, this.id_u);
+            p_statement.setInt(2, this.id_e);
+	    p_statement.setString(3, "date");
+	    p_statement.setString(4, "time");
+	    p_statement.setDouble(5, this.success);
+	    p_statement.setString(6, "wrong answers");
+            p_statement.executeUpdate();
+            ResultSet rs = p_statement.getGeneratedKeys();
+
+            if (rs.next()) {
+                this.id_p = rs.getInt(1);
+            }
+            
+            return true;
+
+	    }
+        }  
+	catch (SQLException sqle) 
+	{
+	    System.out.println("ERREUR");
+	    sqle.printStackTrace();
+	}
 	
 	return false;
     }
@@ -131,6 +146,27 @@ public class Practice {
     {
 	Connection connection = bs.getConnection();
 	
+	try {
+	    if (User.findById(this.id_u, bs) != null && Exercise.findById(id_e, bs) != null)
+	    {
+                String query = "UPDATE Practice SET (id_u = ? , id_e = ? , execution_date = ? , execution_time = ? , success = ? , wrong_answers = ?) WHERE id_p = ?";
+                PreparedStatement p_statement = connection.prepareStatement(query);
+                p_statement.setInt(1, this.id_u);
+                p_statement.setInt(2, this.id_e);
+                p_statement.setString(3, "date");
+                p_statement.setString(4, "time");
+                p_statement.setDouble(5, this.success);
+                p_statement.setString(6, "wrong answers");
+                p_statement.setInt(7, this.id_p);
+                p_statement.executeUpdate();
+	    }
+        }  
+	catch (SQLException sqle) 
+	{
+	    System.out.println("ERREUR");
+	    sqle.printStackTrace();
+	}
+	
 	return false;
     }
     
@@ -138,16 +174,94 @@ public class Practice {
     {
 	Connection connection = bs.getConnection();
 	
+	try {
+            if (Practice.findById(this.getId_p(),bs) != null) {
+                String query = "DELETE FROM Practice WHERE id_p = ?";
+                PreparedStatement p_statement = connection.prepareStatement(query);
+                p_statement.setInt(1, this.id_p);
+                p_statement.executeUpdate();
+            }
+        } 
+	catch (SQLException sqle) 
+	{
+	    System.out.println("ERREUR");
+	    sqle.printStackTrace();
+	}
+	
 	return false;
     }
     
-    public static Practice findById(int idu, int ide, BaseSetting bs)
+    public static Practice findById(int id_p, BaseSetting bs)
     {
 	Connection connection = bs.getConnection();
 	
 	Practice practice = null;
 	
+	try {
+            String query = "SELECT * FROM Practice WHERE id_p = ?";
+            PreparedStatement p_statement = connection.prepareStatement(query);
+            p_statement.setInt(1, id_p);
+
+            ResultSet rs = p_statement.executeQuery();
+
+            if (rs.next()) {
+                int idp = rs.getInt("id_p");
+                int idu = rs.getInt("id_u");
+                int ide = rs.getInt("id_e");
+                String execd = rs.getString("execution_date");
+                String exect = rs.getString("execution_time");
+                double s = rs.getDouble("success");
+                String wa = rs.getString("wrrong_answers");
+
+                /*constructeur*/
+            }
+
+        }  
+	catch (SQLException sqle) 
+	{
+	    System.out.println("ERREUR");
+	    sqle.printStackTrace();
+	}
+	
 	return practice;
+    }
+    
+        public static ArrayList<Practice> findByIds(int id_u, int id_e, BaseSetting bs)
+    {
+	Connection connection = bs.getConnection();
+	
+	ArrayList<Practice> al_practice = new ArrayList<>();
+	
+	try {
+            String query = "SELECT * FROM Practice WHERE id_u = ? AND id_e = ?";
+            PreparedStatement p_statement = connection.prepareStatement(query);
+            p_statement.setInt(1, id_u);
+	    p_statement.setInt(2, id_e);
+	    
+            ResultSet rs = p_statement.executeQuery();
+
+            while (rs.next()) {
+                int idp = rs.getInt("id_p");
+                int idu = rs.getInt("id_u");
+                int ide = rs.getInt("id_e");
+                String execd = rs.getString("execution_date");
+                String exect = rs.getString("execution_time");
+                double s = rs.getDouble("success");
+                String wa = rs.getString("wrrong_answers");
+
+                /*constructeur*/
+		
+		al_practice.add(null);
+            }
+
+        }  
+	catch (SQLException sqle) 
+	{
+	    System.out.println("ERREUR");
+	    sqle.printStackTrace();
+	}
+	
+	return al_practice;
     }
     
 }
