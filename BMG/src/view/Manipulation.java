e3ba02a9293fd56417ea05b1a8284c79744550ad
blaise <blaise@blaise-p6-2050fr>
2014-03-04 14:21:57 +0100

    Correction d'un bug au démarrage (NULL POINTER SI XAMPP NON LANCE)

--- BMG/src/view/Manipulation.java
+++ BMG/src/view/Manipulation.java
@@ -17,47 +17,55 @@ import java.util.logging.Logger;
  */
 public class Manipulation {
     //Class just with static method.
-    
+
     private String label = "Not Connected yet";
 
     public Manipulation() {
 
     }
 
-   /* public String getLabel() {
-        return label;
-    }
+    /* public String getLabel() {
+     return label;
+     }
 
-    public void setLabel(String label) {
-        this.label = label;
-    }*/
-    
-    
-
+     public void setLabel(String label) {
+     this.label = label;
+     }*/
     public static String[] getAllSchoolName(BaseSetting bs) {
-        
-        
+
         if (bs == null) {
             System.out.println("school - BS NULL");
         } else {
-              
+
+            //On initialise la liste des écoles
             ArrayList<String> list = new ArrayList<String>();
             try {
                 //
-                String query = "SELECT name_sch from School";
-                bs.setStatement(bs.getConnection().createStatement());
-                bs.setResult_set(bs.getStatement().executeQuery(query));
 
-                while (bs.getResult_set().next()) {
-                    list.add(bs.getResult_set().getString("name_sch"));
+                //Si la connexion à la base est correcte
+                if (bs.testerConnexion()) {
+                    
+                    //SQL
+                    String query = "SELECT name_sch from School";
+                    bs.setStatement(bs.getConnection().createStatement());
+                    bs.setResult_set(bs.getStatement().executeQuery(query));
+
+                    //Parcours des résultats
+                    while (bs.getResult_set().next()) {
+                        list.add(bs.getResult_set().getString("name_sch"));
+                    }
+                    
+                    //Mise sous la forme d'un tableau
+                    String res[] = new String[list.size()];
+
+                    //Remplissage
+                    for (int i = 0; i < list.size(); i++) {
+                        res[i] = list.get(i);
+                    }
+
+                    return res;
                 }
-                String res[] = new String[list.size()];
 
-                for (int i = 0; i < list.size(); i++) {
-                    res[i] = list.get(i);
-                }
-
-                return res;
             } catch (SQLException ex) {
                 Logger.getLogger(Manipulation.class.getName()).log(Level.SEVERE, null, ex);
             }
