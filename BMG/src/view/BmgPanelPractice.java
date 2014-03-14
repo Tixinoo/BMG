24a63718e5151110baa3cb7e035e7c997736d2b1
Tixinoo <antoine.nosal@gmail.com>
2014-03-14 11:46:58 +0100

    Corrige l'affichage des mauvaises réponses

--- BMG/src/view/BmgPanelPractice.java
+++ BMG/src/view/BmgPanelPractice.java
@@ -91,8 +91,8 @@ public class BmgPanelPractice {
             //Affichage
             panCenter.add(new BmgLabel("<br/><br/>C'est fini"
                     + "<br/><br/>Score : " + practice.getSuccess() + "%"
-                    + "<br/><br/>Mauvaise(s) réponse(s) : " + practice.getWrong_answersString()+ "%"
-                    + "<br/>Temps d'execution : " + practice.getExecution_time() + "sec"
+                    + "<br/><br/>Mauvaise(s) réponse(s) : " + practice.getWrong_answersString()
+                    + "<br/><br/>Temps d'execution : " + practice.getExecution_time() + "sec"
                     + "<br/><br/>Date d'execution : " + practice.getExecution_dateString(), "white", 20));
 
         } else {
